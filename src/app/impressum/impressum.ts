165167b649902b998bff41049a11e0a9bc355b83
Fabian Feichter <me@fabian-feichter.at>
2026-06-16 14:25:54 +0200

    Added copy to clipboard button for my adress

--- src/app/impressum/impressum.ts
+++ src/app/impressum/impressum.ts
@@ -4,10 +4,24 @@ import { RouterLink } from '@angular/router';
 import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 import { AnalyticsService } from '../analytics/AnalyticsService';
 import { AnalyticsCategory } from '../config/analytics';
+import { MatIcon } from '@angular/material/icon';
+import { MatFabButton, MatIconButton } from '@angular/material/button';
+import { CdkCopyToClipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-impressum',
-  imports: [FabianEmailAddress, RouterLink, MatCard, MatCardHeader, MatCardTitle, MatCardContent],
+  imports: [
+    FabianEmailAddress,
+    RouterLink,
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+    MatIcon,
+    MatFabButton,
+    CdkCopyToClipboard,
+    MatIconButton,
+  ],
   templateUrl: './impressum.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './impressum.css',
@@ -15,4 +29,8 @@ import { AnalyticsCategory } from '../config/analytics';
 export class Impressum {
   analyticsService: AnalyticsService = inject(AnalyticsService);
   readonly AnalyticsCategory = AnalyticsCategory;
+
+  protected address() {
+    return 'Fabian Feichter\n' + 'Heinzelgasse 13/7\n' + '9020 Klagenfurt\n' + 'Österreich';
+  }
 }
